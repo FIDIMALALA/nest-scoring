6b2f846c5d2604615dace953d9a4ea7a0af9b723
FIDIMALALA <ismael.fidimalala@gmail.com>
2021-11-14 17:32:20 +0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const APP_PORT = 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(APP_PORT);
+}
+bootstrap();
