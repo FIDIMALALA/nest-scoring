9cbac3cdc7070d8348a3c720794ada6cee53b360
FIDIMALALA <ismael.fidimalala@gmail.com>
2021-11-14 17:48:25 +0300

    Configure hot reloading

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 
 const APP_PORT = 3000;
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(APP_PORT);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
