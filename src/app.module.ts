d12c1f8b10de017ead2b52c127230c2e79b6d22f
FIDIMALALA <ismael.fidimalala@gmail.com>
2021-11-24 21:25:18 +0300

    Create student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [StudentModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
